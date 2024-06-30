a3e18fe993e150f467f2215189801b5ee497b1b8
Mohammed Molla <mjmolla@gmail.com>
2024-06-30 13:10:17 -0400

    intial

---
+++ src/main/java/dataType/MyFirstJava.java
@@ -0,0 +1,15 @@
+package dataType;
+
+public class MyFirstJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		
+		System.out.println("Faruq Welcome to Java world 1");
+		System.out.println("Faruq Welcome to Java world 2");
+		System.out.println("Faruq Welcome to Java world 3");
+		System.out.println("Faruq Welcome to Java world 4");
+		
+	}
+
+}
